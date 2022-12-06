73d942d4becfb9487a1851d826c9d1570096c12b
Trung Do <hondaomauxanh@yahoo.com>
2022-12-06 16:33:23 +0700

    txt file

---
+++ 114-4b71b0edd5ce105aea25.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksds_archive_website=self.webpackChunksds_archive_website||[]).push([[114],{3114:function(e,s,a){a.r(s);var i=a(5900),n=a.n(i),t=a(257),r=a(9541),c=a(1312);const f=t.forwardRef(((e,s)=>{let{bsPrefix:a,variant:i,animation:t,size:f,as:l="div",className:u,...d}=e;a=(0,r.vE)(a,"spinner");const p=`${a}-${t}`;return(0,c.tZ)(l,{ref:s,...d,className:n()(u,p,f&&`${p}-${f}`,i&&`text-${i}`)})}));f.displayName="Spinner",s.default=f}}]);
+//# sourceMappingURL=114-4b71b0edd5ce105aea25.js.map
